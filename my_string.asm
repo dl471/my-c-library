12b200f1589c67f13818fb041fae6aed521f2918
dl471 <annamika@bk.ru>
2018-02-22 22:24:41 +0000

    Implement memcpy

--- my_string.asm
+++ my_string.asm
@@ -2,6 +2,8 @@ GLOBAL _my_strlen
     EXPORT _my_strlen
 GLOBAL _my_memset
     EXPORT _my_memset
+GLOBAL _my_memcpy
+	EXPORT _my_memcpy
 
 section .text
 
@@ -41,4 +43,31 @@ _my_memset:
 .end:
 	pop eax;                     ;Retrieve pointer to start of string
 
+	ret
+	
+_my_memcpy:
+	cmp dword [esp+0x0C], 0x00   ;Immediately return if no bytes are to be copied
+	je .end
+	
+	push dword [esp+4]           ;Create copy of pointer to destination string
+	push esi
+	push edi
+	
+	mov edi, dword [esp+0x10]    ;Destination string
+	mov esi, dword [esp+0x14]    ;Source string
+	mov eax, dword [esp+0x18]    ;Number of elements to be copied
+	
+.write_element:
+	mov cl, byte [esi]
+	mov byte [edi], cl
+	inc esi
+	inc edi
+	dec eax
+	jnz .write_element
+
+	pop edi
+	pop esi
+	pop eax                      ;Retrieve pointer to destination string
+	
+.end:
 	ret
--- my_string.h
+++ my_string.h
@@ -1,2 +1,3 @@
-extern int __cdecl my_strlen(const char *str);
-extern void * memset(void *str, int c, size_t n);
+extern size_t __cdecl my_strlen(const char *str);
+extern void * __cdecl my_memset(void *str, int c, size_t n);
+extern void * __cdecl my_memcpy(void *dest, const void *src, size_t n);
