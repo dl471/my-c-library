5a93b2ebb777883a77589c40c0dd6f01316eb28a
dl471 <annamika@bk.ru>
2018-02-22 23:27:20 +0000

    Convert whitespace from tabs to spaces

--- my_string.asm
+++ my_string.asm
@@ -3,78 +3,80 @@ GLOBAL _my_strlen
 GLOBAL _my_memset
     EXPORT _my_memset
 GLOBAL _my_memcpy
-	EXPORT _my_memcpy
+    EXPORT _my_memcpy
 
 section .text
 
 _my_strlen:
-	push esi
-	
-	xor eax, eax
-	mov esi, [esp+0x08]
-	
-.check:	
-	cmp byte [esi], 0x00
-	je .end
-	inc eax
-	inc esi
-	jmp .check	
-	
-.end:	
-	pop esi
-	
-	ret
-	
+    push esi
+    
+    xor eax, eax
+    mov esi, [esp+0x08]
+    
+.check:    
+    cmp byte [esi], 0x00
+    je .end
+    inc eax
+    inc esi
+    jmp .check    
+    
+.end:    
+    pop esi
+    
+    ret
+    
 _my_memset:
-	cmp dword [esp+0x0C], 0x00   ;Immediately return if no bytes are to be copied
-	je .premature_end
-	
-	mov eax, dword [esp+0x04]    ;Start of pointer to write elements to
-	mov ecx, dword [esp+0x08]    ;Element to be written
-	mov edx, dword [esp+0x0C]    ;Number of elements to be written
-	
-	push eax                     ;Create copy of pointer to start of string
-	
+    cmp dword [esp+0x0C], 0x00   ;Immediately return if no bytes are to be copied
+    je .premature_end
+    
+    mov eax, dword [esp+0x04]    ;Start of pointer to write elements to
+    mov ecx, dword [esp+0x08]    ;Element to be written
+    mov edx, dword [esp+0x0C]    ;Number of elements to be written
+    
+    push eax                     ;Create copy of pointer to start of string
+    
 .write_element:
-	mov byte [eax], cl
-	inc eax
-	dec edx
-	jnz .write_element
-	
-	pop eax;                     ;Retrieve pointer to start of string
+    mov byte [eax], cl
+    inc eax
+    dec edx
+    jnz .write_element
+    
+    pop eax;                     ;Retrieve pointer to start of string
 
-	ret
+    ret
 
 .premature_end:
     mov eax, dword [esp+0x04]
-	ret
-	
+    ret
+    
 _my_memcpy:
-	cmp dword [esp+0x0C], 0x00   ;Immediately return if no bytes are to be copied
-	je .premature_end
-	
-	push dword [esp+4]           ;Create copy of pointer to destination string
-	push esi
-	push edi
-	
-	mov edi, dword [esp+0x10]    ;Destination string
-	mov esi, dword [esp+0x14]    ;Source string
-	mov eax, dword [esp+0x18]    ;Number of elements to be copied
-	
+    cmp dword [esp+0x0C], 0x00   ;Immediately return if no bytes are to be copied
+    je .premature_end
+    
+    push dword [esp+4]           ;Create copy of pointer to destination string
+    push esi
+    push edi
+    
+    mov edi, dword [esp+0x10]    ;Destination string
+    mov esi, dword [esp+0x14]    ;Source string
+    mov eax, dword [esp+0x18]    ;Number of elements to be copied
+    
 .write_element:
-	mov cl, byte [esi]
-	mov byte [edi], cl
-	inc esi
-	inc edi
-	dec eax
-	jnz .write_element
+    mov cl, byte [esi]
+    mov byte [edi], cl
+    inc esi
+    inc edi
+    dec eax
+    jnz .write_element
 
-	pop edi
-	pop esi
-	pop eax                      ;Retrieve pointer to destination string
-	
-	ret
-	
+    pop edi
+    pop esi
+    pop eax                      ;Retrieve pointer to destination string
+    
+    ret
+    
 .premature_end:
     mov eax, dword [esp+0x04]
-	ret
+    ret
+    
+
